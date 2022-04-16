bf0a4045ea50c113b94c083040c385e49861bb38
Jed Dryer <dryer.jed@gmail.com>
2022-04-16 13:47:11 -0500

    View Menu Mostly Working

--- holiday_manager.py
+++ holiday_manager.py
@@ -1,16 +1,19 @@
 import json
 import requests
 import os
+import calendar
+import time
+import random
 
 import datetime as dt
 
 from bs4 import BeautifulSoup
 from dataclasses import dataclass
-from time import sleep
 
 clear_term = 'cls||clear'
 
 ## May add an exit note that makes a suggestion based on the weather
+## May add an exit ASCII Logo if time allows
 
 # -------------------------------------------
 # Modify the holiday class to 
@@ -39,6 +42,7 @@ class WeatherReport:
         self.__current_weather = 'Raining Cats and Dogs'
         self.__country = self.get_locale()
         #self.__current_weather = self.check_weather('current')
+        # Uncomment above line when program is ready
         
     # def __post_init__(self):
     #     self.__locale, self.__country = self.get_locale()
@@ -148,7 +152,7 @@ class HolidayList:
         # Make sure holidayObj is an Holiday Object by checking the type
         # Use innerHolidays.append(holidayObj) to add holiday
         # print to the user that you added a holiday
-        if isinstance(holiday_object, Holiday):
+        if not isinstance(holiday_object, Holiday):
             print(self.__errors[0])
             return
         holidate = holiday_object.date
@@ -186,16 +190,10 @@ class HolidayList:
             self.add_holiday(holiday_object)
         
     def read_json(self):
-        # Read in things from json file location
-        # Use addHoliday function to add holidays to inner list.
-        
-        # Jeds Steps
-        # 1. Check for file
-        # 2. load existing
-        # 3. convert existing to holiday objects
-        # 4. store holiday objects in holiday dictionary
-        # 5. return to main
-        
+        """ 
+        This function check for existing files and loads base
+        holidays if there are no existing files.
+        """
         try:
             if 'managed_holidays.json' in os.listdir():
                 print('Loading Managed Holidays...'.center(78, " "))
@@ -264,14 +262,14 @@ class HolidayList:
         # If yes, use your getWeather function and display results
         pass
 
+
+############## GENERAL FUNCTIONS ##############
+##-------------------------------------------## 
+
+#.............    GET FUNCTIONS  .............#
+
 def get_datetime():
     return(dt.datetime.now())        
-
-def clean_screen():
-    os.system(clear_term)
-    
-def delay(duration):
-    sleep(duration)
         
 def get_templates():
     templates = requests.get(
@@ -285,8 +283,44 @@ def get_errors():
     ).text.split(",")
     return errors      
     
-def display_menu_template(active_menu, current_weather, current_day_info, locale_info):
+def get_month_number(text_month):
+    # There is a better way to do this, but I'm doing it this way.
+    month_dict ={
+        'January': 1,
+        'February': 2, 
+        'March': 3,
+        'April': 4,
+        'May': 5,
+        'June': 6,
+        'July': 7,
+        'August': 8,
+        'September': 9,
+        'October': 10,
+        'November': 11,
+        'December': 12
+    }
+    return month_dict[text_month]    
+    
+def get_week_nums(year, month):
+    end_of_month = calendar.monthrange(year, month)[1]
+    starting_week = dt.datetime(year, month, 1).isocalendar()[1]
+    ending_week = dt.datetime(year, month, end_of_month).isocalendar()[1]
+    
+    # This tries to build a list that will be empty if starting week > ending week
+    res = list(range(starting_week, ending_week + 1))
+    if not res:
+        week_num = 52 if starting_week != 53 else 53
+        res = list(range(starting_week, week_num + 1)) + list(range(1, ending_week + 1))
+    return res    
+    
+    
+#.............   MENU FUNCTIONS  .............#    
+      
+def display_menu_template(active_menu, arg_list):
     clean_screen()
+    current_weather = arg_list[0]
+    current_day_info = arg_list[1]
+    locale_info = arg_list[2]
     print(
         templates[1].format(
         current_menu = active_menu, 
@@ -296,6 +330,83 @@ def display_menu_template(active_menu, current_weather, current_day_info, locale
         )
     )
 
+    
+def holiday_view_builder():
+    print('  Leave blank for current week.')
+    which_year = input("  Which year would you like to view? >> ")
+    
+    if which_year.strip() != "":
+        year_passed = check_input(which_year, 'year', [1950, 2050]) 
+        if not year_passed:
+            return False
+        which_year = int(which_year)
+        month_passed = False
+        week_passed = False
+        while not month_passed:
+            print('\n  To search by week number, input the week number.')
+            print('  Otherwise, input the name of a month.')
+            which_month = input("  Which month of the year? (i.e. Jan, august) >> ")
+            if which_month.isnumeric():
+                if int(which_month) in range(1,54):
+                    try:
+                        week_choice = match_week_to_date(which_month, which_year)
+                        week_choice = list(map(lambda x: convert_dt(x), [week_choice[0], week_choice[1]]))
+                        month_passed = True
+                        return week_choice
+                    except:
+                        print('That week does not seem to be an option. Please try again.')
+                else:
+                    print(' That week is not an option. Please try again.')
+            else:
+                which_month = check_month(which_month)
+                if which_month:
+                    month_passed = True
+                    month_num = get_month_number(which_month)
+                    week_nums = get_week_nums(which_year, month_num)
+                    translate = True if week_nums[0] > week_nums[1] else False
+                    week_dict = {}
+                    print('\n   #: Date Range','\n   ``````````````')
+                    for i, week in enumerate(week_nums):
+                        if (i == 0 and translate):
+                            dates = match_week_to_date(week, which_year - 1)
+                        else:
+                            dates = match_week_to_date(week, which_year)
+                        week_dict[str(week)] = list(map(lambda x: convert_dt(x), [dates[0], dates[1]]))
+                        
+                        dates = " to ".join(str(_.month) + "-" + str(_.day) for _ in dates)
+                        print(f'{str(week).rjust(4)}: {dates}')
+                    example = random.choice([_ for _ in week_dict.keys()])
+                    week_choice = input(f'\n  Choose a week: (i.e. "{example}") >> ')
+                    if (week_choice.isnumeric() and week_choice in week_dict.keys()):
+                        return week_dict[week_choice]
+                    else:
+                        if '"' in week_choice:
+                            print('  Hmmm... Try leaving out the " " marks. That was just an example. ')
+                            delay(2)
+                        else:
+                            print("  Hmmm... That just doesn't compute.\n  Starting Infinite Loop...")
+                            delay(2)
+                            print("  Wheeew, done. That was exhausting. Good thing you got a fancy processor.")
+                            delay(2)
+        # Once the month has passed get the month number and determine which weeks are in the month
+        # for every week in the month print a menu_string consisting of the week_num, date_range
+        # ask the user to pick their chosen week
+        # The weeknumber will be used to determine which holidays to display, so holidays will be matched to a week number    
+    
+def convert_dt(date_time_object):
+    return dt.datetime.strftime(date_time_object, '%G-%m-%d')
+
+def match_week_to_date(week_num, year):
+    date_string = f'{year}-V{int(week_num)}-1'
+    start_date = dt.datetime.strptime(date_string, "%G-V%V-%w").date()
+    end_date = start_date + dt.timedelta(days=6.9)
+    return start_date, end_date
+
+def delay(duration):
+    time.sleep(duration)
+    
+def clean_screen():
+    os.system(clear_term)
     
 def modify_current_date_time():
     current_dt = get_datetime()
@@ -321,15 +432,43 @@ def modify_current_date_time():
     day_info = f'{current_date} | {time_of_day}'
     return day_info
 
+#.............  CHECK FUNCTIONS  .............#
+
 def check_input(input_string, requirements, limits):
     if requirements == 'number':
         if (input_string.isnumeric() and int(input_string) in range(1, limits + 1)):
             return True
-
+    elif requirements == 'year':
+        if (input_string.isnumeric() and int(input_string) in range(limits[0], limits[1])):
+            return True
     print(f'  {input_string} is not a valid {requirements}. Please try again.')
     delay(1.5)
     return False
+
+def check_month(month_input):
+    allowed_months = [
+        'Jan', 'January', 'Feb', 'February', 'Mar', 'March', 'Apr', 
+        'April', 'May', 'May', 'Jun', 'June', 'Jul', 'July', 'Aug', 
+        'August', 'Sept', 'September', 'Oct', 'October', 'Nov', 'November', 
+        'Dec', 'December'
+    ]
+    month_converter = [
+        'January', 'February', 'March', 'April', 'May', 
+        'June', 'July', 'August', 'September', 'October', 
+        'November', 'December'
+    ]
+    if month_input.capitalize() not in allowed_months:
+        print('  That month is not recognized. Please try again. ')
+        return False
+    else:
+        month_input = month_input.capitalize()
+        if month_input in month_converter:
+            matched = month_input
+        else:
+            matched = allowed_months[allowed_months.index(month_input) + 1]       
+    return matched
     
+
     
 def main():
     #locale_info, country = get_locale()
@@ -339,12 +478,12 @@ def main():
     locale_info, current_weather = CurrentWeather.return_data()
     BoontaEve = HolidayList(errors)
     delay(2)
-    
+    main_args = [current_weather, current_day_info, locale_info]
     outer_passed = False
     while not outer_passed:
         passed = False
         while not passed:
-            display_menu_template('Main Menu', current_weather, current_day_info, locale_info)
+            display_menu_template('Main Menu', main_args)
             print(templates[2])
             main_menu_choice = input('  Please Choose an Option >> ')
             passed = check_input(main_menu_choice, 'number', 5)
@@ -356,7 +495,12 @@ def main():
         elif main_menu_choice == 3:
             pass
         elif main_menu_choice == 4:
-            pass
+            passed = False
+            while not passed:
+                display_menu_template('Holiday Viewer', main_args)
+                passed = holiday_view_builder()
+                    
+                
         elif main_menu_choice == 5:
             return
         
@@ -381,7 +525,7 @@ if __name__ == "__main__":
     main()
     
     # Closing Sequence
-    display_menu_template('Closing','','','')
+    display_menu_template('Closing',['','',''])
     print("\n" * 2)
     print('Closing the Manager'.center(78," "))
     delay(1.5)
@@ -408,6 +552,13 @@ if __name__ == "__main__":
 # and substitute the placeholders 
 # for example: filetxt.format(fname = "John", age = 36)
 # This will make your code far more readable, by seperating text from code.
+
+
+
+
+
+
+
 
 
 
